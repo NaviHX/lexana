0dea9db580f421234085dcf28d644b2104c7f280
navihx <932942142@qq.com>
2021-09-29 22:42:08 +0800

    解决越界问题

--- src/keyword.cpp
+++ src/keyword.cpp
@@ -1,12 +1,13 @@
 #include "keyword.h"
+#include <vector>
 
-char *keyword[] = {"auto","double","int","struct","break","else","long","switch","case","enum","register",
+std::vector<std::string> keyword = {"auto","double","int","struct","break","else","long","switch","case","enum","register",
         "typedef","char","extern","return","union","const","float","short","unsigned","continue","for",
          "signed","void","default","goto","sizeof","volatile","do","while","static","if"};
 
 bool isKeyword(const std::string& s)
 {
-    for(int i=0;i<sizeof(keyword);i++)
+    for(int i=0;i<keyword.size();i++)
     {
         if(s==keyword[i])
             return true;
